ac10468306f97fb98420f7430bbd91c27f869e03
Chandrakant Bothe <42034218+bothe@users.noreply.github.com>
2020-11-16 10:36:54 +0100

    Update dialogue_act_annotator.py

--- dialogue_act_annotator.py
+++ dialogue_act_annotator.py
@@ -43,7 +43,7 @@ text = lists_to_text(speaker_ids, utterances, utt_ids, emotions)
 
 # Send request to the resting server
 try:
-    link = "http://4f82241c0f4c.eu.ngrok.io/"
+    link = "http://a5d1dc6019be.eu.ngrok.io/"
     results = requests.post(link + 'predict_das', json={"text": text}).json()['result']
 
     # convert a long string of the resulted text back to lists
